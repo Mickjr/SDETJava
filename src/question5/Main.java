5e8c0a05c0f7635340a254ff35f5860457530124
Manton Horton <mantonhorton@gmail.com>
2016-02-02 01:27:39 -0500

    added question5

---
+++ src/question5/Main.java
@@ -0,0 +1,16 @@
+package question5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num1 = 5;
+		double num2 = 10;
+		Math mymath = new Math();
+		System.out.println(mymath.sum(num1, num2));
+		System.out.println(mymath.difference(num1, num2));
+		System.out.println(mymath.product(num1, num2));
+		System.out.println(mymath.quot(num1, num2));
+	}
+
+}
---
+++ src/question5/Math.java
@@ -0,0 +1,17 @@
+package question5;
+
+public class Math {
+
+		public double sum(double x, double y) {
+			return x + y;
+		}
+		public double difference(double x, double y) {
+			return x - y;
+		}
+		public double product(double x, double y) {
+			return x * y;
+		}
+		public double quot(double x, double y) {
+			return x / y;
+		}
+}
