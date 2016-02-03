0fa8b28689d1667cac5dd28cde1da39812702901
Manton Horton <mantonhorton@gmail.com>
2016-02-02 21:09:42 -0500

    completed question 6

--- src/question6/ForEach.java
+++ src/question6/ForEach.java
@@ -7,6 +7,7 @@ public class ForEach {
 		int a = 6;
 		int b = 2;
 		int c = 5;
+		int d = 8;
 		
 		//If/Else
 		if(a>7){
@@ -37,6 +38,40 @@ public class ForEach {
 			c++;
 		}
 		
+		//Do/While Loop
+		do{
+			System.out.println(d);
+			d++;
+		}while(d < 10);
+		
+		//For loop
+		for(int e = 0; e < 10; e++)
+		{
+			System.out.println(e);
+		}
+		
+		//Enhanced For loop
+		int[] items ={1,2,3,4,5};
+		for(int element : items)
+		{
+			System.out.println(element);
+		}
+		
+		//Break
+		for(int f = 0; f < 10; f++){
+			if(f == 5)break;
+			System.out.println(f);
+		}
+		System.out.println("Finish Loop!");
+		
+		//Continue
+		for(int i = 0; i < 10; i++)
+		{
+			if(i == 5)continue;
+			System.out.println(i);
+			
+		}
+			
 	}
 
 }
