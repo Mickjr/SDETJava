7f0020f9034ae304a97e172bc1fc87d0983c0cb7
Manton Horton <mantonhorton@gmail.com>
2016-02-02 22:32:43 -0500

    finished question 7

---
+++ src/question7/CustomerClass.java
@@ -0,0 +1,39 @@
+package question7;
+
+public class CustomerClass {
+	
+	private static final float MYFLOAT = 3.14f;
+	
+	public CustomerClass(){
+		
+	}
+	
+	public CustomerClass(int x){
+		
+	}
+	
+	public void myMethod(){
+		
+	}
+	
+	public void myMethod(int x, String str){
+		
+	}
+	
+	static int counter;
+	
+	static{
+		counter = 0;
+	}
+	
+	public static void myMethod2(){
+		
+	}
+	
+	public static void myMethod3(float f){
+		f = MYFLOAT;
+	}
+	
+	
+	
+}
